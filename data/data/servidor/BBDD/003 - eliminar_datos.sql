204f0e0b80738169b940347a3fb0fe79b9311068
JoseMi-1222 <josearras343@gmail.com>
2025-12-01 18:39:47 +0100

    Delete data/data directory

--- data/data/servidor/BBDD/003 - eliminar_datos.sql
+++
@@ -1,5 +0,0 @@
-DELETE FROM asignatura
-DELETE FROM aula
-DELETE FROM curso 
-DELETE FROM profesor 
-DELETE FROM horario 
--- data/data/servidor/app-horario/src/main/java/com/ies/poligono/sur/app/horario/processor/HorarioServiceProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.ies.poligono.sur.app.horario.processor;
-
-import com.ies.poligono.sur.app.horario.dto.PostImportacionInputDTO;
-
-public interface HorarioServiceProcessor {
-
-	public void importarHorario(PostImportacionInputDTO obj);
-
-}
